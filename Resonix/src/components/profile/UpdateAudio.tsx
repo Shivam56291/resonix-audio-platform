8de89052255426f4b8358f3a06de848d405d5418
Shivam Kumar <roysinghaniashivam@gmail.com>
2026-02-24 23:47:29 +0530

    feat: add update audio API integration with poster upload support

--- Resonix/src/components/profile/UpdateAudio.tsx
+++ Resonix/src/components/profile/UpdateAudio.tsx
@@ -1,7 +1,14 @@
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { ProfileNavigatorStackParamList } from 'src/@types/navigation';
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import AudioForm from 'components/form/AudioForm';
+import { useDispatch } from 'react-redux';
+import { useQueryClient } from '@tanstack/react-query';
+import { getClient } from 'api/client';
+import { mapRange } from 'utils/math';
+import { updateNotification } from 'store/notification';
+import catchAsyncError from 'api/catchError';
+import { NavigationProp, useNavigation } from '@react-navigation/native';
 
 type Props = NativeStackScreenProps<
   ProfileNavigatorStackParamList,
@@ -10,6 +17,56 @@ type Props = NativeStackScreenProps<
 
 const UpdateAudio: FC<Props> = ({ route }) => {
   const { audio } = route.params;
+  const [uploadProgress, setUploadProgress] = useState(0);
+  const [busy, setBusy] = useState(false);
+  const dispatch = useDispatch();
+  const { navigate } =
+    useNavigation<NavigationProp<ProfileNavigatorStackParamList>>();
+  const queryClient = useQueryClient();
+
+  const handleUpdate = async (formData: FormData) => {
+    setUploadProgress(0);
+
+    setTimeout(() => {
+      setBusy(true);
+    }, 120);
+    try {
+      const client = await getClient({
+        'Content-Type': 'multipart/form-data',
+      });
+
+      await client.put(`/audio/${audio.id}`, formData, {
+        onUploadProgress: progressEvent => {
+          const progress = mapRange({
+            inputValue: progressEvent.loaded,
+            outputMin: 0,
+            outputMax: 100,
+            inputMax: progressEvent.total || 1,
+            inputMin: 0,
+          });
+          setUploadProgress(Math.floor(progress));
+        },
+      });
+      dispatch(
+        updateNotification({
+          message: 'Audio updated successfully',
+          type: 'success',
+        }),
+      );
+      setUploadProgress(100);
+      setBusy(false);
+
+      queryClient.invalidateQueries({ queryKey: ['latest-uploads'] });
+      queryClient.invalidateQueries({ queryKey: ['recommended'] });
+      queryClient.invalidateQueries({ queryKey: ['uploads-by-profile'] });
+      navigate('Profile');
+    } catch (error: any) {
+      const errorMessage = catchAsyncError(error);
+      dispatch(updateNotification({ message: errorMessage, type: 'error' }));
+      setBusy(false);
+      setUploadProgress(0);
+    }
+  };
   return (
     <AudioForm
       initialValues={{
@@ -17,9 +74,9 @@ const UpdateAudio: FC<Props> = ({ route }) => {
         about: audio.about,
         category: audio.category,
       }}
-      onSubmit={data => {
-        console.log(data);
-      }}
+      onSubmit={handleUpdate}
+      busy={busy}
+      progress={uploadProgress}
     />
   );
 };
